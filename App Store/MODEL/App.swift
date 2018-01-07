b46d489a7b3c9a0f4e3ebe181176206445371f4a
Chidi Emeh <chidiemeh184@gmail.com>
2018-01-06 22:37:57 -0500

    API Data Struct, Codable

---
+++ App Store/MODEL/App.swift
@@ -0,0 +1,78 @@
+//
+//  App.swift
+//  App Store
+//
+//  Created by Chidi Emeh on 1/6/18.
+//  Copyright © 2018 Chidi Emeh. All rights reserved.
+//
+
+import Foundation
+
+
+//App as defined in the Apple API
+
+struct App : Codable {
+    
+    var ipadScreenshotsUrls : [String]?
+    var appletvScreenshotUrls : [String]?
+    var artworkUrl60 : String?
+    var artworkUrl512 : String?
+    var artworkUrl100 : String?
+    var artistViewUrl : String?
+    var isGameCenterEnabled : Bool?
+    var kind : String?
+    var features : [String]?
+    var supportedDevices : [String]?
+    var screenshotUrls : [String]?
+    var advisories : [String]?
+    var averageUserRatingForCurrrentVersion : Float?
+    var trackCensoredName : String?
+    var languageCodesISO2A : [String]?
+    var fileSizeBytes : String?
+    var contentAdvisoryRating : String?
+    var userRatingCountForCurrentVersion : Int?
+    var trackViewUrl : String?
+    var trackContentRating : String?
+    var primaryGenreId : Int?
+    var sellerName : String?
+    var genreIds : [String]?
+    var currentVersionReleaseDate : String?
+    var currency : String?
+    var wrapperType : String?
+    var version : String?
+    var description : String?
+    var artistId : Int?
+    var artistName : String?
+    var genre : [String]?
+    var price : Double?
+    var trackId : Int?
+    var trackName : String?
+    var bundleId : String?
+    var isVppDeviceBasedLicensingEnabled : Bool?
+    var primaryGenreName : String?
+    var releaseDate : Date?
+    var minimumOsVersion : String?
+    var formattedPrice : String?
+    var releaseNotes : String?
+    var averageUserRating : Float?
+    var userRatingCount : Int?
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
