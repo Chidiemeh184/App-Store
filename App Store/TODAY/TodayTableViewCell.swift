c882977e0713218403c4e26e72fead85e159349e
Chidi Emeh <chidiemeh184@gmail.com>
2018-01-11 13:11:05 -0500

    Shadow of TableViewCells

--- App Store/TODAY/TodayTableViewCell.swift
+++ App Store/TODAY/TodayTableViewCell.swift
@@ -24,12 +24,13 @@ class TodayTableViewCell: UITableViewCell {
         
         appView.layer.borderColor = UIColor.clear.cgColor
         appView.layer.shadowColor =  UIColor(red: 179/255, green: 179/255, blue: 179/255, alpha: 1).cgColor
-        appView.layer.shadowOffset = CGSize(width: 0, height: 0)  //Default 0, 5
-        appView.layer.shadowRadius = 15  //Default 5
+        appView.layer.shadowOffset = CGSize(width: 0, height: 4)  //Default 0, 5
+        appView.layer.shadowRadius = 25  //Default 5
         appView.layer.shadowOpacity = 1.0
         appView.layer.masksToBounds = false
         appView.layer.cornerRadius = 15   //Default 8
         appView.layer.borderWidth = 0.5
+
         //appView.layer.shadowPath = UIBezierPath(roundedRect:self.bounds, cornerRadius:self.contentView.layer.cornerRadius).cgPath
     }
     
