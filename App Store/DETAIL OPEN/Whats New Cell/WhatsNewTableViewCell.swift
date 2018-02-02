6be6dba7c64bc7728eabc1308d68bbe9b1c8a0ee
Chidi Emeh <chidiemeh184@gmail.com>
2018-02-01 22:33:09 -0500

    SDWebimage

--- App Store/DETAIL OPEN/Whats New Cell/WhatsNewTableViewCell.swift
+++ App Store/DETAIL OPEN/Whats New Cell/WhatsNewTableViewCell.swift
@@ -13,11 +13,12 @@ class WhatsNewTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.selectionStyle = .none
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
 
